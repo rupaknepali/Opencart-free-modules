22bf19af28267f78b3a5486cb462fb902d82f164
Rupak <webocreation.com@gmail.com>
2019-08-24 17:15:09 -0700

    Out of Stock Free Modules

---
+++ OutOfStock/upload/admin/language/en-gb/extension/module/outofstock.php
@@ -0,0 +1,26 @@
+<?php
+
+// Heading
+$_['heading_title'] = 'Out Of Stock Label';
+
+$_['text_module'] = 'Modules';
+$_['text_success'] = 'Success: You have modified Out Of Stock Label module!';
+$_['text_edit'] = 'Edit Out Of Stock Label Module';
+$_['text_please_select'] = 'Please Select...';
+
+// Entry
+$_['entry_status'] = 'Status';
+$_['entry_outofstock'] = 'Label';
+$_['entry_title'] = 'Label';
+
+$_['entry_outofstock_show_marker_in_product_page'] = 'Show Label In Product Page';
+$_['entry_load_default_style'] = 'Load Default CSS Style';
+$_['entry_css_nodes'] = 'CSS Nodes';
+$_['entry_confirm_default_style_loading'] = 'Loading the default style makes you lose current style. Load default style?';
+$_['entry_css_nodes_content'] = '.product-not-available - .product-not-available p - .not-available-mark';
+$_['entry_style'] = 'CSS Style';
+$_['entry_placeholder'] = 'Insert label here';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Out Of Stock Label module!';
+$_['error_empty_line'] = 'Warning: Empty Line!';
