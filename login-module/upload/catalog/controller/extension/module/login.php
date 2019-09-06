30d22a5fd1c49be187fc95dfab32f6664d62c00d
Rupak <webocreation.com@gmail.com>
2019-09-05 19:32:34 -0700

    Single Instance module login module frontend added

---
+++ login-module/upload/catalog/controller/extension/module/login.php
@@ -0,0 +1,44 @@
+<?php
+class ControllerExtensionModuleLogin extends Controller {
+	public function index() {
+		$this->load->language('extension/module/login');
+
+		$data['logged'] = $this->customer->isLogged();
+		
+		$data['action'] = $this->url->link('account/login', '', true);
+		$data['register'] = $this->url->link('account/register', '', true);
+		$data['forgotten'] = $this->url->link('account/forgotten', '', true);
+
+		if (isset($this->request->post['redirect']) && (strpos($this->request->post['redirect'], $this->config->get('config_url')) !== false || strpos($this->request->post['redirect'], $this->config->get('config_ssl')) !== false)) {
+			$data['redirect'] = $this->request->post['redirect'];
+		} elseif (isset($this->session->data['redirect'])) {
+			$data['redirect'] = $this->session->data['redirect'];
+
+			unset($this->session->data['redirect']);
+		} else {
+			$data['redirect'] = '';
+		}
+
+		if (isset($this->session->data['success'])) {
+			$data['success'] = $this->session->data['success'];
+
+			unset($this->session->data['success']);
+		} else {
+			$data['success'] = '';
+		}
+
+		if (isset($this->request->post['email'])) {
+			$data['email'] = $this->request->post['email'];
+		} else {
+			$data['email'] = '';
+		}
+
+		if (isset($this->request->post['password'])) {
+			$data['password'] = $this->request->post['password'];
+		} else {
+			$data['password'] = '';
+		}
+
+		return $this->load->view('extension/module/login', $data);
+	}
+}
