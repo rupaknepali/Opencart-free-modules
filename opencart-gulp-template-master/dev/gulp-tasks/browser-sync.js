87729c55b1b564c3c9651d271c2abd0bb3a4022a
Rupak Nepali <webocreation.com@gmail.com>
2022-06-05 11:52:06 -0700

    Removing Gulp opencart template because of Critical issues to be fix, fix is coming soon

--- opencart-gulp-template-master/dev/gulp-tasks/browser-sync.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(gulp, plugins) {
-    return function() {
-        plugins.browserSync.init({
-        proxy: "http://localhost/gulp-oc/",
-        notify: true
-      });
-    };
-};
