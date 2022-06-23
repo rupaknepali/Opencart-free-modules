dc04214d1ace1694fcb8f39afecdbc51b7be8e9c
Rupak Nepali <webocreation.com@gmail.com>
2022-06-22 21:13:48 -0700

    Webocreation Opencart 4 custom theme

---
+++ WeboCreation-Opencart-4-custom-theme/webocreation4/admin/language/en-gb/theme/theme_standard.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title'] = 'Webocreation Theme';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Success: You have modified the Webocreation theme!';
+$_['text_edit'] = 'Edit Webocreation Theme';
+
+// Entry
+$_['entry_status'] = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify the Webocreation theme!';
---
+++ WeboCreation-Opencart-4-custom-theme/webocreation4/catalog/controller/startup/theme_standard.php
@@ -0,0 +1,23 @@
+<?php
+namespace Opencart\Catalog\Controller\Extension\webocreation4\Startup;
+
+class ThemeStandard extends \Opencart\System\Engine\Controller
+{
+    public function index(): void
+    {
+        if ($this->config->get('theme_standard_status')) {
+            $this->event->register('view/*/before', new \Opencart\System\Engine\Action('extension/webocreation4/startup/theme_standard|event'));
+        }
+    }
+
+    public function event(string &$route, array &$args, mixed &$output): void
+    {
+        $override = [
+            'common/header',
+        ];
+
+        if (in_array($route, $override)) {
+            $route = 'extension/webocreation4/' . $route;
+        }
+    }
+}
